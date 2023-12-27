487499523d66008d145239288a33ac4c91ba78c1
AnonimowyCoder <wiktor.leskiewicz27@gmail.com>
2023-12-27 16:51:09 +0100

    zmiana 2

--- header.hpp
+++ header.hpp
@@ -1,1 +1,16 @@
 #pragma once
+#ifndef naglowek
+#define naglowek
+
+//dodanie bibliotek
+#include<iostream>
+#include<string>
+#include<vector>
+#include<que>
+#include<algorithm>
+
+using namespace std::string, std::cout, std::cin, std::endl;
+
+
+
+#endif // !naglowek
